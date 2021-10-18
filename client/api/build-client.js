6c94efc6688bf7af0d52c4b9b9ef157da3c83fdf
Sergio Mercado <smercadocelaya@gmail.com>
2021-10-18 12:10:29 -0700

    update base url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export const buildClient = ({ req }) => {
     if (typeof window === 'undefined') {
         // We are on the server
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'http://www.smcelaya.com',
             headers: req.headers
         });
     } else {
